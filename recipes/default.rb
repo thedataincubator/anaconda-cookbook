23f2e18c24f64c7c5b3a76f26a31c5071d0f6fca
Dylan Bargteil <dylan.bargteil@gmail.com>
2018-02-15 12:50:44 -0500

    change md5 to sha256

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,7 @@
 remote_file "#{Chef::Config[:file_cache_path]}/Miniconda.sh" do
   source "https://repo.continuum.io/miniconda/Miniconda3-4.3.31-Linux-x86_64.sh"
   mode "777"
-  checksum "df2f9770d83df8269f3f43f1e60285e6"
+  checksum "5551f01f436b6409d467412c33e12ecc4f43b5e029290870f8fdeca403c274e6"
 end
 
 file "/etc/profile.d/conda.sh" do
