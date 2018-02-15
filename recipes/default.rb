bfa2208764a92853ab614f4a99c42073980626b3
Dylan Bargteil <dylan.bargteil@gmail.com>
2018-02-15 10:58:35 -0500

    switch to miniconda3 & update major version

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,7 @@
 remote_file "#{Chef::Config[:file_cache_path]}/Miniconda.sh" do
-  source "https://repo.continuum.io/miniconda/Miniconda-3.10.1-Linux-x86_64.sh"
+  source "https://repo.continuum.io/miniconda/Miniconda3-4.3.31-Linux-x86_64.sh"
   mode "777"
-  checksum "363f56f5608d1552325549e7371fcf460c5ed45484eb300058e3b99c997808b5"
+  checksum "df2f9770d83df8269f3f43f1e60285e6"
 end
 
 file "/etc/profile.d/conda.sh" do
